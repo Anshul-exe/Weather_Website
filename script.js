844e8f63aa9259f57286fb9d4d4559826cf495f8
anshul-exe <anshulchauhan1224@gmail.com>
2024-03-27 03:38:16 +0530

    added code

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
-const inputBox = document.querySelector('.input-box');
-const searchBtn = document.getElementById('searchBtn');
-const weather_img = document.querySelector('.weather-img');
-const temperature = document.querySelector('.temperature');
-const description = document.querySelector('.description');
-const humidity = document.getElementById('humidity');
-const wind_speed = document.getElementById('wind_speed');
-const location_not_found = document.querySelector('.location-not-found');
-const weather_body = document.querySelector('.weather-body');
+const inputBox = document.querySelector(".input-box");
+const searchBtn = document.getElementById("searchBtn");
+const weather_img = document.querySelector(".weather-img");
+const temperature = document.querySelector(".temperature");
+const description = document.querySelector(".description");
+const humidity = document.getElementById("humidity");
+const wind_speed = document.getElementById("wind_speed");
+const location_not_found = document.querySelector(".location-not-found");
+const weather_body = document.querySelector(".weather-body");
 
-async function checkWeather(city){
-    const api_key = "087ad834898d7bd54cd5cce65cbecab7"
-    const url = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${api_key}`
+async function checkWeather(city) {
+  const api_key = "087ad834898d7bd54cd5cce65cbecab7";
+  const url = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${api_key}`;
 
-    const weather_data = await fetch('${url}').then(respone => response.json());
+  const weather_data = await fetch("${url}").then((respone) => response.json());
 
-    if(weather_data.cod === '404'){
-        location_not_found.style.display = "flex";
-        weather_body.style.display = "none";
-        console.log("error");
-        return;
-    }
-    location_not_found.style.display = "none";
-    weather_body.style.display = "flex";
+  if (weather_data.cod === "404") {
+    location_not_found.style.display = "flex";
+    weather_body.style.display = "none";
+    console.log("error");
+    return;
+  }
+  location_not_found.style.display = "none";
+  weather_body.style.display = "flex";
 
-    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
-    description.innerHTML = `${weather_data.weather[0].description}`;
-    humidity.innerHTML = `${weather_data.main.humidity}%`;
-    wind_speed.innerHTML = `${weather_data.wind.speed}Km/h`;
+  temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+  description.innerHTML = `${weather_data.weather[0].description}`;
+  humidity.innerHTML = `${weather_data.main.humidity}%`;
+  wind_speed.innerHTML = `${weather_data.wind.speed}Km/h`;
 
-    switch(weather_data.weather[0].main){
-        case 'Clouds':
-            weather_img.src = "/assets/clouds.png";
-            break;
-        case 'Clear':
-            weather_img.src = "/assets/clear.png";
-            break;
-        case 'Rain':
-            weather_img.src = "/assets/rain.png";
-            break;
-        case 'Mist':
-            weather_img.src = "/assets/mist.png";
-            break;
-        case 'Snow':
-            weather_img.src = "/assets/snow.png";
-            break;
-    }
+  switch (weather_data.weather[0].main) {
+    case "Clouds":
+      weather_img.src = "/assets/clouds.png";
+      break;
+    case "Clear":
+      weather_img.src = "/assets/clear.png";
+      break;
+    case "Rain":
+      weather_img.src = "/assets/rain.png";
+      break;
+    case "Mist":
+      weather_img.src = "/assets/mist.png";
+      break;
+    case "Snow":
+      weather_img.src = "/assets/snow.png";
+      break;
+  }
 }
-searchBtn.addEventListener('click',()=>{
-    checkWeather(inputBox.value);
-})
+searchBtn.addEventListener("click", () => {
+  checkWeather(inputBox.value);
+});
